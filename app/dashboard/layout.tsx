f3a8cfb7f9f423c7019cafb529fff58f11f8073d
LostoVayne <moises.barrera.dev@gmail.com>
2025-02-16 17:04:00 -0300

    feat: add Sidebar in LandingPage

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,7 +12,6 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         <Authenticated>
           <Sidebar />
         </Authenticated>
-
         <div className="flex-1 flex flex-col min-w-0">
           <Header />
           <main className="flex-1 overflow-y-auto">{children}</main>
