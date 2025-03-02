10b3e9829876b787524f9a5703b0ed88c65043ee
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-02 01:37:51 -0300

    chore: actualizar dependencias y ajustar componentes
    - Actualizar versiones de paquetes en bun.lock y package.json
    - Añadir altura fija al contenedor del botón CTA en la página de inicio
    - Implementar funcionalidad básica en el componente ChatRow
    - Corregir importaciones y añadir contexto de navegación

--- components/chatrow.tsx
+++ components/chatrow.tsx
@@ -2,6 +2,11 @@
 
 import { Id } from "@/convex/_generated/dataModel";
 import { Chat } from "@/convex/chats";
+import { NavigationContext } from "@/lib/context/navigation";
+import { TrashIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { use } from "react";
+import { Button } from "./ui/button";
 
 interface ChatRowProps {
   chat: Chat;
@@ -9,5 +14,31 @@ interface ChatRowProps {
 }
 
 export const ChatRow = ({ chat, onDelete }: ChatRowProps) => {
-  return <div></div>;
+  // Implement Hooks react
+  const router = useRouter();
+  const { closeMobileNav } = use(NavigationContext);
+
+  const handleClick = () => {
+    router.push(`/dashboard/chat/${chat._id}`);
+    closeMobileNav();
+  };
+
+  return (
+    <div
+      className="group rounded-xl border border-gray-200/30 bg-white/50 backdrop-blur-sm hover:bg-white/80 transition-all duration-200 cursor-pointer shadow-sm hover:shadow-md"
+      onClick={handleClick}
+    >
+      <div className="p-4">
+        <div className="flex justify-between items-start">
+          Chat
+          <Button>
+            <TrashIcon className="size-4 text-gray-400 hover:text-red-500 transition-colors duration-300" />
+          </Button>
+        </div>
+        {/* Last Message */}
+
+        {/* {lastMessage && <p></p>} */}
+      </div>
+    </div>
+  );
 };
